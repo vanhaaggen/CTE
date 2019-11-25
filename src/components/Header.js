5cdc8f038f9bd8172c1dc6d73a7a298371ff0ab2
vanhaaggen <vanhaaggen@gmail.com>
2019-11-25 13:40:57 +0100

    add onload animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React from 'react'
 import { Animated } from 'react-animated-css'
 import './styles/Header.sass'
 
-import cteLogo from '../images/crosstransparent.png'
-
+import logo from '../images'
+const { back, barbell, circle, kettlebell, laurel } = logo
 
 const BlankStripe = () => {
     return <>
@@ -18,34 +18,38 @@ const Header = () => {
         <header className='header' id="header">
             <div className="animation-container">
                 <Animated
-                    animationIn="bounceInLeft"
+                    animationIn="bounceIn"
+                    animationInDelay={1200}
                     isVisible={true}
                     className="animation">
-                    <img className="header-img" src={cteLogo} alt="cte-logo" />
+                    <img className="header-img" src={back} alt="cte-logo" />
                 </Animated>
                 <Animated
                     animationIn="bounceInLeft"
+                    animationInDelay={900}
                     isVisible={true}
                     className="animation">
-                    <img className="header-img" src={cteLogo} alt="cte-logo" />
+                    <img className="header-img" src={circle} alt="cte-logo" />
                 </Animated>
                 <Animated
-                    animationIn="bounceInLeft"
+                    animationIn="bounceInRight"
+                    animationInDelay={600}
                     isVisible={true}
                     className="animation">
-                    <img className="header-img" src={cteLogo} alt="cte-logo" />
+                    <img className="header-img" src={laurel} alt="cte-logo" />
                 </Animated>
                 <Animated
-                    animationIn="bounceInLeft"
+                    animationIn="bounceInUp"
+                    animationInDelay={300}
                     isVisible={true}
                     className="animation">
-                    <img className="header-img" src={cteLogo} alt="cte-logo" />
+                    <img className="header-img" src={barbell} alt="cte-logo" />
                 </Animated>
                 <Animated
-                    animationIn="bounceInLeft"
+                    animationIn="bounceInDown"
                     isVisible={true}
                     className="animation">
-                    <img className="header-img" src={cteLogo} alt="cte-logo" />
+                    <img className="header-img" src={kettlebell} alt="cte-logo" />
                 </Animated>
             </div>
         </header>
