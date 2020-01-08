0330acf9c40f434a8dedfb93d155376445904559
vanhaaggen <vanhaaggen@gmail.com>
2020-01-08 12:06:09 +0100

    change pkg-lock amd update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,22 +9,45 @@ import { I18NConsumer } from '../i18ncontext'
 
 export default function Footer() {
 
+    const legal = () => {
+        const date = new Date()
+        const year = date.getFullYear()
+        return <>
+            <p className="footer-legal--text">Crosstraining Empordà, {year}</p>
+            <p>Tots els drets reservats</p>
+        </>
+    }
+
     return <>
 
         <I18NConsumer>
             {({ text, lang }) => (
-                <div className="footer-container">
-                    <p className="footer-name"><span className="footer-name--brand1">{text.footer.brand1.toUpperCase()}</span><span className="footer-name--brand2">{text.footer.brand2.toUpperCase()}</span></p>
-                    <div className="footer-media-container">
+                <>
+                    <div className="footer-container">
+                        <div className="footer-container-upside">
 
-                        <a href="https://www.instagram.com/crosstrainingemporda/?hl=es"><FontAwesomeIcon className="footer-insta media" icon={faInstagram} /></a>
-                        <a href="https://www.facebook.com/crosstrainingemporda/"><FontAwesomeIcon className="footer-face media" icon={faFacebook} /></a>
+                            <p className="footer-name"><span className="footer-name--brand1">{text.footer.brand1.toUpperCase()}</span><span className="footer-name--brand2">{text.footer.brand2.toUpperCase()}</span></p>
 
+                            <div className="footer-right-wrapper">
+                                <p className="footer-right-text">segueix-nos:</p>
+                                <div className="media-container">
+                                    <a href="https://www.instagram.com/crosstrainingemporda/?hl=es"><FontAwesomeIcon className="footer-insta media" icon={faInstagram} /></a>
+                                    <div className="media-separator"></div>
+                                    <a href="https://www.facebook.com/crosstrainingemporda/"><FontAwesomeIcon className="footer-face media" icon={faFacebook} /></a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="footer-legal">
+                            {legal()}
+                        </div>
 
                     </div>
-                </div>
+
+                </>
             )}
         </I18NConsumer>
+
 
     </>
 
