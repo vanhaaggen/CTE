66c85551eadc05d138cd387c69aa1af3f2610ac2
vanhaaggen <vanhaaggen@gmail.com>
2020-01-28 21:58:01 +0100

    add table programatically and hook it with text file

--- src/components/Schedule/index.js
+++ src/components/Schedule/index.js
@@ -6,10 +6,33 @@ import './style.sass'
 export default function (props) {
     const [widthState, setWidthState] = useState({ fontSize: "2rem" })
     const { text, lang } = props
+    const { sectionSchedule } = text[`${lang}`]
 
     useEffect(() => {
         setWidthState({ fontSize: "6rem" })
     }, [])
+
+
+    const createTable = () => {
+        let table = []
+        let cellId = [0]
+
+        for (let i = 0; i < 9; i++) {
+            let children = []
+
+            for (let j = 0; j < 7; j++) {
+                children.push(<td className="table-data">
+                    <div className={`table-data-child cell${cellId[cellId.length - 1]}`}>
+                        {sectionSchedule[`row${i + 1}`][`cell${cellId[cellId.length - 1]}`]}
+                    </div>
+                </td>)
+
+                cellId.push((cellId.length - 1) + 1)
+            }
+            table.push(<tr className={`row${i + 1}`}>{children}</tr>)
+        }
+        return table
+    }
 
     return <>
 
@@ -23,14 +46,18 @@ export default function (props) {
             <p className="schedule-title section-title-wrapper">{text[`${lang}`].navbar.schedule.toUpperCase()}</p>
         </div>
 
-
-        <div>
-            <article>
-                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit euismod proin, mauris himenaeos praesent posuere vehicula nascetur ante interdum velit pellentesque, neque nam nulla sodales sed felis cum molestie. Metus ullamcorper tortor magna commodo mauris enim suscipit consequat, bibendum nec nascetur lectus praesent facilisi. Montes ornare ad orci lobortis bibendum, neque diam eget scelerisque cursus condimentum, donec semper mauris accumsan.</p>
-            </article>
-        </div>
-
-
+        <table>
+            <tr>
+                <th></th>
+                <th>Dilluns</th>
+                <th>Dimarts</th>
+                <th>Dimecres</th>
+                <th>Dijous</th>
+                <th>Divendres</th>
+                <th>Dissabte</th>
+            </tr>
+            {createTable()}
+        </table>
 
 
     </>
