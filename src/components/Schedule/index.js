fe50114f34c5fbfc2fae699cccaeb0632082ca12
vanhaaggen <vanhaaggen@gmail.com>
2020-01-29 12:37:05 +0100

    update

--- src/components/Schedule/index.js
+++ src/components/Schedule/index.js
@@ -12,21 +12,28 @@ export default function (props) {
         setWidthState({ fontSize: "6rem" })
     }, [])
 
+    /**
+     * Creates Schedule table and load dinamicaly content from the text prop.
+     * 
+     * @param {number} rows
+     * @param {number} cols
+     * 
+     * @returns {Array}
+     */
 
-    const createTable = () => {
+    const createTable = (rows, cols) => {
         let table = []
         let cellId = [0]
 
-        for (let i = 0; i < 9; i++) {
+        for (let i = 0; i < rows; i++) {
             let children = []
-
-            for (let j = 0; j < 7; j++) {
-                children.push(<td className="table-data">
-                    <div className={`table-data-child cell${cellId[cellId.length - 1]}`}>
-                        {sectionSchedule[`row${i + 1}`][`cell${cellId[cellId.length - 1]}`]}
-                    </div>
-                </td>)
-
+            for (let j = 0; j < cols; j++) {
+                children.push(
+                    <td className="table-data">
+                        <div className={`table-data-child cell${cellId[cellId.length - 1]}`}>
+                            {sectionSchedule[`row${i + 1}`][`cell${cellId[cellId.length - 1]}`]}
+                        </div>
+                    </td>)
                 cellId.push((cellId.length - 1) + 1)
             }
             table.push(<tr className={`row${i + 1}`}>{children}</tr>)
@@ -45,20 +52,20 @@ export default function (props) {
         }}>
             <p className="schedule-title section-title-wrapper">{text[`${lang}`].navbar.schedule.toUpperCase()}</p>
         </div>
-
-        <table>
-            <tr>
-                <th></th>
-                <th>Dilluns</th>
-                <th>Dimarts</th>
-                <th>Dimecres</th>
-                <th>Dijous</th>
-                <th>Divendres</th>
-                <th>Dissabte</th>
-            </tr>
-            {createTable()}
-        </table>
-
+        <div className="table-container">
+            <table>
+                <tr>
+                    <th></th>
+                    <th>Dilluns</th>
+                    <th>Dimarts</th>
+                    <th>Dimecres</th>
+                    <th>Dijous</th>
+                    <th>Divendres</th>
+                    <th>Dissabte</th>
+                </tr>
+                {createTable(9, 7)}
+            </table>
+        </div>
 
     </>
 }
