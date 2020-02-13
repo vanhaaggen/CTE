6dcd9af00f1d08b8a4a28250117f331ff243ffc8
vanhaaggen <vanhaaggen@gmail.com>
2020-02-13 20:47:17 +0100

    make # and @ linkable

--- src/utils/addClassToText.js
+++ src/utils/addClassToText.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+const INSTA_HASHTAG_URL = "https://www.instagram.com/explore/tags/"
+const INSTA_ATTAG_URL = "https://www.instagram.com/"
+
 /**
  * Iterates over a String to search for "#" and "@" and adds  a classname
  * 
@@ -11,24 +14,33 @@ import React from 'react'
 export default function (string) {
     let strg = string.split(' ')
 
-    console.log('function runs')
     for (let i = 0; i < strg.length; i++) {
         if (strg[i][0] === "#") {
-            strg[i] = React.createElement('span', {
+
+            strg[i] = React.createElement('a', {
                 key: [i],
                 className: 'hashtag',
+                href: `${INSTA_HASHTAG_URL}${strg[i].substring(1)}/`,
+                target: 'blank',
                 style: { color: 'blue' }
             }, ` ${strg[i]}`)
+
         } else if (strg[i][0] === "@") {
-            strg[i] = React.createElement('span', {
+
+            strg[i] = React.createElement('a', {
                 key: [i],
                 className: 'atTag',
+                href: `${INSTA_ATTAG_URL}${strg[i].substring(1)}/`,
+                target: "blank",
                 style: { color: 'purple' }
             }, ` ${strg[i]} `)
+
         } else {
+
             strg[i] = React.createElement('span', {
                 key: [i]
             }, ` ${strg[i]}`)
+
         }
     }
 
