01c4dd9d6e52bf1370bcef893c6c6e8989e6881a
vanhaaggen <vanhaaggen@gmail.com>
2020-03-12 20:51:55 +0100

    delete animations

--- src/components/SectionLayout.js
+++ src/components/SectionLayout.js
@@ -1,30 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import { Animated } from 'react-animated-css'
 import isInViewPort from '../utils/isInViewport'
 import SectionTitle from './SectionTitle'
 import './SectionStyle.sass'
 
 
 export default function SectionLayout(props) {
-    const [isInView, setIsInView] = useState(false)
     const { section, content } = props
 
 
-    useEffect(() => {
-        function watchScroll() {
-            window.addEventListener('scroll', function (event) {
-                if (isInViewPort(`#${section}`)) setIsInView(true)
-            })
-        }
-
-        watchScroll()
-        //remove listener (like componentWillUnmount)
-        return () => {
-            window.removeEventListener("scroll", function (event) {
-                isInViewPort(`#${section}`)
-            })
-        }
-    })
 
 
     return (
@@ -32,17 +15,8 @@ export default function SectionLayout(props) {
             <section className={`${section}-background`} >
 
                 <SectionTitle props={props} section={section} />
+                <div className={`container-${section}`}>{content}</div>
 
-                <div className={`container-${section}`}>
-                    {isInView && <>
-                        <Animated
-                            animationIn="fadeIn"
-                            animationInDelay={300}
-                            isVisible={true}>
-                            {content}
-                        </Animated>
-                    </>}
-                </div>
             </section>
         </>
     )
--- src/components/WhoAreWe/index.js
+++ src/components/WhoAreWe/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Card from './Card'
 
-import { Animated } from 'react-animated-css'
 import './style.sass'
 
 import coach1 from '../../images/coach1.png'
@@ -17,50 +16,37 @@ export default function (props) {
         <section className="who-container-content">
 
             <div className="who-wrapp">
-                <Animated className="who-wrapp__anim"
-                    animationIn="bounceInLeft"
-                    animationInDelay={500}
-                    isVisible={true}>
 
-                    <div className="divisor-background back-border-img-left">
-                        <div className="divisor-wave1">
-                            <div className="who-divisor">
+                <div className="divisor-background back-border-img-left">
+                    <div className="divisor-wave1">
+                        <div className="who-divisor">
 
-                                <article className="who-divisor__paragraph">
-                                    {text[`${lang}`].sectionWho.par1}
-                                    {text[`${lang}`].sectionWho.par2}
-                                </article>
+                            <article className="who-divisor__paragraph">
+                                {text[`${lang}`].sectionWho.par1}
+                                {text[`${lang}`].sectionWho.par2}
+                            </article>
 
-                                <Card image={coach1} text1={text[`${lang}`].sectionWho.coach1.name} text2={text[`${lang}`].sectionWho.coach1.text} />
+                            <Card image={coach1} text1={text[`${lang}`].sectionWho.coach1.name} text2={text[`${lang}`].sectionWho.coach1.text} />
 
-                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="divisor-background back-border-img-right">
+                    <div className="divisor-wave2">
+                        <div className="who-divisor">
+
+                            <Card image={coach2} text1={text[`${lang}`].sectionWho.coach2.name} text2={text[`${lang}`].sectionWho.coach2.text} />
+
+                            <article className="who-divisor__paragraph par3">
+                                <q className='who-divisor__paragraph--size-par3'>{text[`${lang}`].sectionWho.par3}</q>
+                            </article>
 
                         </div>
                     </div>
-                </Animated>
+                </div>
 
-
-
-                <Animated className="who-wrapp__anim"
-                    animationIn="bounceInRight"
-                    animationInDelay={1100}
-                    isVisible={true}>
-
-
-                    <div className="divisor-background back-border-img-right">
-                        <div className="divisor-wave2">
-                            <div className="who-divisor">
-
-                                <Card image={coach2} text1={text[`${lang}`].sectionWho.coach2.name} text2={text[`${lang}`].sectionWho.coach2.text} />
-
-                                <article className="who-divisor__paragraph par3">
-                                    <q className='who-divisor__paragraph--size-par3'>{text[`${lang}`].sectionWho.par3}</q>
-                                </article>
-
-                            </div>
-                        </div>
-                    </div>
-                </Animated>
             </div>
         </section>
     )
